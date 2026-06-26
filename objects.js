1b64cd1ba0ac28d43575d0cc94a08f19746fac60
Dani0619 <daniacode123@gmail.com>
2026-06-26 20:55:36 +0530

    object is finished

--- objects.js
+++ objects.js
@@ -1,8 +1,21 @@
 //objects declarations
 let car={
-  name='Nexon',
-  driveTrain='petrol,ev,diesel',
-  price=20,
-  brand='Tata'
-}
+  name:'Nexon',
+  driveTrain:'petrol,ev,diesel',
+  price:20,
+  brand:'Tata',
+
+  onRoad(){
+    return this.price += 2
+  }
+};
 console.log(car)
+//select specifically
+console.log(car.price)
+//to addproperty
+car['Model']=2024
+console.log(car)
+//to update
+car.Model=2025
+console.log(car)
+console.log(car.onRoad())
