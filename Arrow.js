05fad877c38eb5f05f9a4f2277714e0dbca09124
Dani0619 <daniacode123@gmail.com>
2026-06-30 11:16:54 +0530

    Arrow

--- Arrow.js
+++ Arrow.js
@@ -5,7 +5,14 @@
 //say()
 
 //function expression
-let say=function hi(){
-    console.log("From function expression")
+//let say=function hi(){
+//    console.log("From function expression")
+//}
+//say()
+
+
+//Anonymns function
+let say1=function(){
+    console.log("From Anonymns function")
 }
-say()
+say1()
